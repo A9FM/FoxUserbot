9ac6ecab6ccf289cc64294e3702e447da02dadd4
A9FM <68541852+A9FM@users.noreply.github.com>
2022-01-21 19:55:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import logging
 from pyrogram import Client
-from colorama import Fore
+
+logging.basicConfig(filename="fox_userbot.log", filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
 plugins = dict(root="plugins")
-
 Client("my_account", plugins=plugins).run()

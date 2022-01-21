550cc6f39880ab718f00b2e7e6a49e139d5201f7
A9FM <68541852+A9FM@users.noreply.github.com>
2022-01-21 22:49:19 +0200

    Add files via upload

---
+++ plugins/example.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from plugins.settings.main_settings import module_list, file_list
+
+from prefix import my_prefix
+prefix = my_prefix()
+
+@Client.on_message(filters.command("example_edit", prefixes=prefix) & filters.me)
+async def example_edit(client: Client, message: Message):
+    await message.edit("<code>This is an example module</code>")
+
+
+module_list['Example'] = f'{prefix}example_edit'
+file_list['Example'] = 'example.py'
--- plugins/find_id.py
+++ plugins/find_id.py
@@ -9,6 +9,6 @@ prefix = my_prefix()
 @Client.on_message(filters.command('find_id', prefixes=prefix) & filters.me)
 async def find_id(client: Client, message: Message):
   await message.edit(f'**ID of this chat: ** ```{message.chat.id}```')
-  
+
 module_list['FindIDThisChat'] = f'{prefix}fing_id'
 file_list['FindIDThisChat'] = 'find_id.py'

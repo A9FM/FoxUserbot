5e83d9b0b9c52f313f607683f7201805ca908266
A9FM <68541852+A9FM@users.noreply.github.com>
2022-01-22 14:50:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ apt install python3-pip -y
 apt install git -y
 apt install wget -y
 pip3 install -U pyrogram
-python3 install.py
+wget https://github.com/FoxUserbot/FoxUserbot/archive/refs/heads/main.zip
+unzip main.zip
+rm main.zip

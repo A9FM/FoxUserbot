fab5fe11268280b755da9921a9c8ed33cb45c746
A9FM <68541852+A9FM@users.noreply.github.com>
2022-01-22 14:39:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 apt update -y
-apt upgrade -y
 apt install python3 -y
 apt install python3-pip -y
 apt install git -y
 apt install wget -y
-pip3 install pyrogram
+pip3 install -U pyrogram
 python3 install.py

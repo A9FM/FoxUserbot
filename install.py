1312785ea7c61425e88f06e9cd19f3a6e60cb10f
A9FM <68541852+A9FM@users.noreply.github.com>
2022-01-23 03:34:42 +0200

    Update install.py

--- install.py
+++ install.py
@@ -11,16 +11,9 @@ for rq in requirements:
     
 os.system("cls" if os.name == "nt" else "clear")
 
-
-def createConfig(path):
-    print(Fore.GREEN + "Добро пожаловать в установщик FoxUserBot.")
-    print(Fore.GREEN + "Сейчас вам предложат ввести номер телефона и код входа от вашего аккаунта в Telegram, сделайте это для установки FoxUserBot.\nПри последующем запуске, пишите в терминал python3 main.py")
-    try:
-        os.system("python3 main.py")
-    except:
-        os.system("python main.py")
-
-if __name__ == "__main__":
-    path = "config.ini"
-    createConfig(path)
-
+print(Fore.GREEN + "Добро пожаловать в установщик FoxUserBot.")
+print(Fore.GREEN + "Сейчас вам предложат ввести номер телефона и код входа от вашего аккаунта в Telegram, сделайте это для установки FoxUserBot.\nПри последующем запуске, пишите в терминал python3 main.py")
+try:
+    os.system("python3 main.py")
+except:
+    os.system("python main.py")

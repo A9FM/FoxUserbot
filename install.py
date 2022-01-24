21b2b6fce1ebacb2520ddaa651bd5c4e75418b76
A9FM <68541852+A9FM@users.noreply.github.com>
2022-01-24 13:47:31 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,26 +1,13 @@
 import os
-from colorama import Fore
 import time
-import configparser
+import pip
 
-os.system("pip3 install -U colorama")
-
-requirements = ["pyrogram", "requests", "wget", "telegraph"]
-for rq in requirements:
-    os.system(f"pip3 install -U {rq}")
+pip.main(["install", "telegraph", "pyrogram", "requests", "wget", "colorama", "--upgrade"])
     
 os.system("cls" if os.name == "nt" else "clear")
 
-
-def createConfig(path):
-    print(Fore.GREEN + "FoxUserBot installed.")
-    print(Fore.GREEN + "Start FoxUserbot... (if you get error, writte 'python3 main.py' in the terminal)")
-    try:
-        os.system("python3 main.py")
-    except:
-        os.system("python main.py")
-
-if __name__ == "__main__":
-    path = "config.ini"
-    createConfig(path)
-
+print("FoxUserBot installed.\nStart FoxUserbot... (if you get error, writte 'python3 main.py' in the terminal)")
+try:
+    os.system("python3 main.py")
+except:
+    os.system("python main.py")

8ed21254a8af50fcb8c9862faf2ba37c5b4eed28
A9FM <68541852+A9FM@users.noreply.github.com>
2022-01-25 02:47:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
 apt update -y
 apt install python3 python3-pip wget -y
-pip3 install -U pyrogram
-
-wget -O foxub.$$ https://github.com/FoxUserbot/FoxUserbot/archive/refs/heads/main.zip && (unzip foxub.$$ && rm foxub.$$ && cd FoxUserbot-main)
-python3 install.py
+wget -O foxub.$$ https://github.com/FoxUserbot/FoxUserbot/archive/refs/heads/main.zip && (unzip foxub.$$ && rm foxub.$$ && cd FoxUserbot-main && python3 install.py)

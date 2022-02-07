c4e3d3c9a6f3ebe24ae07eb8d0b2550bad6b9f7b
A9FM <68541852+A9FM@users.noreply.github.com>
2022-02-08 00:09:28 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,3 +0,0 @@
-apt update -y
-apt install python3 python3-pip wget -y
-wget -O foxub.$$ https://github.com/FoxUserbot/FoxUserbot/archive/refs/heads/main.zip && (unzip foxub.$$ && rm foxub.$$ && cd FoxUserbot-main && python3 install.py)

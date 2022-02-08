bf6ba2119d5c9cd5a62bfa20db54fc7a5cdeb5d9
A9FM <68541852+A9FM@users.noreply.github.com>
2022-02-08 20:08:28 +0300

    fix error for MacOS \ Windows

--- install.py
+++ install.py
@@ -3,4 +3,4 @@ import pip
 
 pip.main(["install", "telegraph", "pyrogram", "requests", "wget", "colorama", "--upgrade"])
 print("\n\nFoxUserBot installed.\nStart FoxUserbot... (if you get error, writte 'python3 main.py' in the terminal)")
-os.system("python3 main.py" if os.name == "nt" else "python main.py")
+os.system("python main.py" if os.name == "nt" else "python3 main.py")

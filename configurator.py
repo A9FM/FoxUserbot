c1f30b89e58f5e80d93035bc6d308275a44d5afc
A9FM <68541852+A9FM@users.noreply.github.com>
2022-06-28 21:16:24 +0300

    bug fix

--- configurator.py
+++ configurator.py
@@ -2,7 +2,7 @@ import os
 import sys
 import configparser
 
-config_path = os.path.join(".", "config.ini")
+config_path = os.path.join(sys.path[0], "config.ini")
 config = configparser.ConfigParser()
 config.read(config_path)
 
@@ -21,21 +21,16 @@ def api():
 def my_api():
     try:
         api_id, api_hash, device_model = api()
-    except:
-        try:
-            os.remove("config.ini")
-        except:
-            pass
+    except Exception as NotFound:
         config.add_section("pyrogram")
         config.set("pyrogram", "api_id", config_id)
         config.set("pyrogram", "api_hash", config_hash)
         config.set("pyrogram", "device_model", config_model)
         with open(config_path, "w") as config_file:
             config.write(config_file)
-
         api_id = config_id
         api_hash = config_hash
         device_model = config_model
-        print(f"Not found config.ini\nGenerating new...")
+        print(f"Not found in config api\nLog: {NotFound}\nGenerating config.ini")
         pass
     return api_id, api_hash, device_model

46a318f4c3e39d512d1306b838480a9c7785a787
ARCHI <ag387737@gmail.com>
2024-07-05 01:55:55 +0300

    press F, webshot.deam.io

--- modules/plugins_1system/webshot.py
+++ modules/plugins_1system/webshot.py
@@ -8,18 +8,13 @@ async def webshot(client, message):
     try:
         user_link = (message.command[1])
         await message.edit("Try create screenshot...")
-        try:
-            full_link = f"https://webshot.deam.io/{user_link}/?width=1920&height=1080?delay=2000?type=png"
-            await client.send_photo(message.chat.id, full_link, caption=f"**Screenshot of the page ⟶** {user_link}")
-        except Exception as dontload:
-            await message.edit(f"Error! {dontload}\nTrying again create screenshot...")
-            full_link = f"https://mini.s-shot.ru/1920x1080/JPEG/1024/Z100/?{user_link.replace('https://', '')}"
-            await client.send_photo(message.chat.id, full_link, caption=f"**Screenshot of the page ⟶** {user_link}")
+        full_link = f"https://mini.s-shot.ru/1920x1080/JPEG/1024/Z100/?{user_link.replace('https://', '')}"
+        await client.send_photo(message.chat.id, full_link, caption=f"**Screenshot of the page ⟶** {user_link}")
         await message.delete()
     except Exception as error:
         await message.delete()
         await client.send_message(
-            message.chat.id, f"**Something went wrong\nLog:{error}...**"
+            message.chat.id, f"**Something went wrong\n\nLog:{error}...**"
         )
 
 
